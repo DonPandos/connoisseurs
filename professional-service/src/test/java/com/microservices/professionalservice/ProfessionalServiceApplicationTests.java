839a9a9de1e4ba7b0bae72e50178f3b50280193e
DonPandos <ewrr_96@mail.ru>
2023-02-08 23:10:17 +0000

    Initial commit

---
+++ professional-service/src/test/java/com/microservices/professionalservice/ProfessionalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.professionalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
