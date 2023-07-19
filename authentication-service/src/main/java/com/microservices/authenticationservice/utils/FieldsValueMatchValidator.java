3dccbff0a0d2a186900b05116898b764d95718b5
somov.a <somov.lesha51@gmail.com>
2023-07-19 13:58:36 +0300

    fix: delete commented code

--- authentication-service/src/main/java/com/microservices/authenticationservice/utils/FieldsValueMatchValidator.java
+++ authentication-service/src/main/java/com/microservices/authenticationservice/utils/FieldsValueMatchValidator.java
@@ -21,24 +21,4 @@ public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValu
         if (fieldValue != null) return fieldValue.equals(fieldMatchValue);
         else return fieldMatchValue == null;
     }
-/*    public boolean isValid(Object value, ConstraintValidatorContext context) {
-        Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(field);
-        Object fieldMatchValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
-
-        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$";
-
-
-        if (fieldValue != null && fieldMatchValue != null && fieldValue instanceof String && fieldMatchValue instanceof String) {
-            String passwordValue = (String) fieldValue;
-            String passwordConfirmValue = (String) fieldMatchValue;
-
-            Pattern pattern = Pattern.compile(regex);
-            Matcher passwordMatcher = pattern.matcher(passwordValue);
-            Matcher confirmMatcher = pattern.matcher(passwordConfirmValue);
-
-            return passwordMatcher.matches() && confirmMatcher.matches() && passwordValue.equals(passwordConfirmValue);
-        } else {
-            return false;
-        }
-    }*/
 }
